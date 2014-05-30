75a1646a21b2785115a2433cdc852bf34092c61d
Shauna <shaunagm@gmail.com>
2014-05-29 21:05:50 -0400

    Modify deploy.sh to move CNAME to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-
+set -e   # if there's an error, stop running
+set -x   # print each command as it runs
 bin/pelican content -s settings.py
 bin/ghp-import output -m "Regenerate site"
+git checkout gh-pages
+echo "mergestories.com" > CNAME
+git add CNAME
+git commit -m "commit CNAME file"
+git checkout master
 git push origin gh-pages
 
