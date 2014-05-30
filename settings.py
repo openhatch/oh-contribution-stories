96ce14a4618d2b6316e2813d57686dd198764867
Shauna <shaunagm@gmail.com>
2014-05-29 23:54:59 -0400

    Change default num of articles shown to 5
    Close #13

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 SITEURL = 'http://openhatch.github.io/oh-contribution-stories'
 TIMEZONE = 'America/New_York'
 
-# Specify a customized theme, via absolute path
-THEME = './themes/pelican-elegant'
+THEME = './themes/pelican-elegant'  # Specify a customized theme, via absolute path
 SITE_DESCRIPTION = 'This site is in progress, please check back later.'
+
+RECENT_ARTICLES_COUNT = 5       # Sets # of articles to be shown on front page
