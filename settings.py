04300d81c6ef7953414c959360e11c432707cc1e
Shauna <shaunagm@gmail.com>
2014-06-10 18:04:29 -0400

    Add OpenHatch name to site, compress tags in index view

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ SITEURL = 'http://mergestories.com'
 TIMEZONE = 'America/New_York'
 
 THEME = './themes/pelican-elegant'  # Specify a customized theme, via absolute path
-SITE_DESCRIPTION = 'This site is in progress, please check back later.'
+
+SITENAME = 'Merge Stories'
+SITE_DESCRIPTION = 'A project of OpenHatch'
 
 RECENT_ARTICLES_COUNT = 5       # Sets # of articles to be shown on front page
