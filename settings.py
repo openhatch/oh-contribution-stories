f2d9fc5ab30bb753d83e797b44dbe21f741bbbaa
Shauna <shaunagm@gmail.com>
2014-06-16 22:04:55 -0400

    Re-design front page

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ THEME = './themes/pelican-elegant'  # Specify a customized theme, via absolute p
 SITENAME = 'Merge Stories'
 SITE_DESCRIPTION = 'A project of OpenHatch'
 
-RECENT_ARTICLES_COUNT = 5       # Sets # of articles to be shown on front page
+RECENT_ARTICLES_COUNT = 10       # Sets # of articles to be shown on front page
 
 DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
 
