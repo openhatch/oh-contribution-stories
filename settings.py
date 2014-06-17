46ea96b66ddbaf429865b25077454a859f1ad264
Shauna <shaunagm@gmail.com>
2014-06-17 11:52:20 -0400

    Tag links to individual tag pages, remove tags.html

--- settings.py
+++ settings.py
@@ -1,15 +1,20 @@
-SITEURL = 'http://mergestories.com'
-TIMEZONE = 'America/New_York'
-
-THEME = './themes/pelican-elegant'  # Specify a customized theme, via absolute path
-
-SITENAME = 'Merge Stories'
-SITE_DESCRIPTION = 'A project of OpenHatch'
-
-RECENT_ARTICLES_COUNT = 10       # Sets # of articles to be shown on front page
-
 DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
 
 PLUGIN_PATH = 'pelican-plugins'
 PLUGINS = ['tipue_search']
+
+RECENT_ARTICLES_COUNT = 10       # Sets # of articles to be shown on front page
+
+SITENAME = 'Merge Stories'
+SITE_DESCRIPTION = 'A project of OpenHatch'
+SITEURL = 'http://mergestories.com'
+
+SUMMARY_MAX_LENGTH = 100
+
+THEME = './themes/pelican-elegant'  # Specify a customized theme, via absolute path
+
+TIMEZONE = 'America/New_York'
+
+
+
 
