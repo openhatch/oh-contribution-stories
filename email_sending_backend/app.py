a89e548357d4962f02a3f3b31e892602ce8e38b3
Asheesh Laroia <asheesh@asheesh.org>
2014-06-17 23:52:48 -0700

    Add more steps keys

--- email_sending_backend/app.py
+++ email_sending_backend/app.py
@@ -29,16 +29,29 @@ def post_to_meaningful_data(d):
     # Now, filter for the key names we want
     input_to_output_keys = {
         'entry.235795032': 'link',
-        'entry.178508708': 'step 2',
         'entry.1389151802': 'step 0',
-        'entry.1954301530': 'step 3',
         'entry.1919080730': 'email',
         'entry.2023565106': 'name',
         'entry.603062675': 'step 1',
         'entry.300556150': 'project',
         'entry.690550334': 'tags',
         'entry.1363522345': 'title',
-        }
+        'entry.178508708': 'step 2',
+        'entry.1954301530': 'step 3',
+        'entry.731379118': 'step 4',
+        'entry.1448384929': 'step 5',
+        'entry.1224977072': 'step 6',
+        'entry.2073139872': 'step 7',
+        'entry.1771949325': 'step 8',
+        'entry.1419921080': 'step 9',
+        'entry.65011868': 'step 10',
+        'entry.122349419': 'step 11',
+        'entry.1515341332': 'step 12',
+        'entry.441925640': 'step 13',
+        'entry.441127551': 'step 14',
+        'entry.1660802530': 'step 15',
+        'entry.807514746': 'step 16',
+    }
 
     step_dict = {}
 
